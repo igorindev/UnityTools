e172bb8ea75718248a2d3fa3cdbfd9ee664a9d0d
igorindev <igorindev@users.noreply.github.com>
2021-02-09 17:22:26 -0300

    Tools

---
+++ Assets/VideoPlayer/ImagePlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ImagePlayerController : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+    [SerializeField] CanvasGroup[] spriteClips;
+
+    [SerializeField] float fadeTime = 0.4f;
+    [SerializeField] float imageDuration = 3;
+    int actual = 0;
+
+    void Start()
+    {
+        actual = 0;
+        canvasGroup = spriteClips[0];
+    }
+
+    public void Initialize()
+    {
+        actual = 0;
+        canvasGroup.alpha = 0;
+        StartCoroutine(Play());
+    }
+
+    void PlayNext()
+    {
+        //Troca o sprite
+        canvasGroup = spriteClips[actual];
+        StartCoroutine(Play());
+    }
+    IEnumerator Play()
+    {
+        float counter = 0;
+
+        while (counter < fadeTime)
+        {
+            counter += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(0, 1, counter / fadeTime);
+
+            yield return null;
+        }
+        canvasGroup.alpha = 1;
+        yield return new WaitForSeconds(imageDuration);
+
+        counter = 0;
+        while (counter < fadeTime)
+        {
+            counter += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(1, 0, counter / fadeTime);
+
+            yield return null;
+        }
+        canvasGroup.alpha = 0;
+        actual += 1;
+
+        if (actual < spriteClips.Length)
+        {
+            PlayNext();
+        }
+    }
+}
---
+++ Assets/VideoPlayer/VideoPlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoPlayerController : MonoBehaviour
+{
+    [SerializeField] VideoClip[] videoClips;
+
+    VideoClip playing;
+    int actual = 0;
+    VideoPlayer videoPlayer;
+    ImagePlayerController imageControll;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        imageControll = GetComponent<ImagePlayerController>();
+        actual = 0;
+        playing = videoClips[0];
+        videoPlayer.clip = playing;
+        StartCoroutine(Play());
+    }
+    void PlayNext()
+    {
+        videoPlayer.clip = playing = videoClips[actual];
+        StartCoroutine(Play());
+    }
+
+    IEnumerator Play()
+    {
+        videoPlayer.Play();
+        yield return new WaitForSeconds((float)playing.length);
+        actual += 1;
+        if (actual < videoClips.Length)
+        {
+            PlayNext();
+        }
+        else
+        {
+            videoPlayer.targetCameraAlpha = 0;
+            imageControll.Initialize();
+        }
+    }
+}
