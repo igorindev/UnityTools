1fee9c0e425035ccae291d83c275017e9ae82204
igorindev <igorindev@users.noreply.github.com>
2021-02-11 14:21:48 -0300

    dont destroy

---
+++ Assets/DontDestroy/DontDestroy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyThis(gameObject);
+    }
+
+    public void DontDestroyThis(GameObject gameObject)
+   {
+       DontDestroyOnLoad(gameObject);
+   }
+    public void DestroyManagers()
+    {
+        Destroy(gameObject);
+    }
+}
