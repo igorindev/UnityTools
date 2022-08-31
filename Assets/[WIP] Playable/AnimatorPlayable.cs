ae0d37f38889f553af2d3c6762667a0af0fd75b9
Igor Lamounier <igorindev@gmail.com>
2022-08-30 23:56:28 -0300

    Update

---
+++ Assets/[WIP] Playable/AnimatorPlayable.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatorPlayable : MonoBehaviour
+{
+    //https://docs.unity3d.com/Manual/Playables-Examples.html
+    //https://forum.unity.com/threads/why-does-the-playables-api-exist-when-should-i-use-it-rather-than-mecanim-animator-controllers.912857/
+
+    [Header("MENU")]
+    public AnimationClip idle;
+
+    [Header("GAMEPLAY")]
+    public AnimationClip idleGame;
+    public AnimationClip running;
+
+    public Animator[] targetAnimators;
+    AnimationMixerPlayable[] mixerPlayable;
+
+    public float weight;
+
+    PlayableGraph playableGraph;
+
+    bool gameplay;
+    public void GenerateGameplayAnimator(Animator[] animators)
+    {
+        targetAnimators = animators;
+        gameplay = true;
+        playableGraph.Destroy();
+        Start();
+    }
+
+    private void OnValidate()
+    {
+        targetAnimators[0] = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        playableGraph = PlayableGraph.Create("Animator Playable");
+
+        //GraphVisualizerClient.Show(playableGraph); Need the graph plugin
+        mixerPlayable = new AnimationMixerPlayable[targetAnimators.Length];
+        for (int i = 0; i < targetAnimators.Length; i++)
+        {
+            //Create the mixer
+            mixerPlayable[i] = AnimationMixerPlayable.Create(playableGraph, 2);
+
+            // Create the outputs
+            AnimationPlayableOutput animationOutput = AnimationPlayableOutput.Create(playableGraph, "Animation", targetAnimators[i]);
+
+            // Create the playables.
+            AnimationClipPlayable idlePlayable = AnimationClipPlayable.Create(playableGraph, gameplay ? idleGame : idle);
+            AnimationClipPlayable runningPlayable = AnimationClipPlayable.Create(playableGraph, running);
+
+            // Connect the playables to an output
+            animationOutput.SetSourcePlayable(mixerPlayable[i]);
+
+            playableGraph.Connect(idlePlayable, 0, mixerPlayable[i], 0);
+            playableGraph.Connect(runningPlayable, 0, mixerPlayable[i], 1);
+        }
+
+        // Plays the Graph.
+        playableGraph.Play();
+    }
+
+    void OnDisable()
+    {
+        // Destroys all Playables and Outputs created by the graph.
+        playableGraph.Destroy();
+    }
+
+    void Update()
+    {
+        weight = Mathf.Clamp01(weight);
+        for (int i = 0; i < mixerPlayable.Length; i++)
+        {
+            mixerPlayable[i].SetInputWeight(0, 1.0f - weight);
+            mixerPlayable[i].SetInputWeight(1, weight);
+        }
+    }
+}
